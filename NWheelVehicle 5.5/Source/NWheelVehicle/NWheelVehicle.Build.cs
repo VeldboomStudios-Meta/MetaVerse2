3377fd206fe2fff4653631d2b68a7a429b7ed824
dejah marshall <2165302@talnet.nl>
2025-02-17 15:44:53 +0100

    Update NWheelVehicle.Build.cs

--- NWheelVehicle 5.5/Source/NWheelVehicle/NWheelVehicle.Build.cs
+++ NWheelVehicle 5.5/Source/NWheelVehicle/NWheelVehicle.Build.cs
@@ -8,7 +8,7 @@ public class NWheelVehicle : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Json","JsonUtilities", "HTTP" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HTTP", "Json", "UMG", "JsonUtilities", "WebBrowser", "Slate", "SlateCore"  });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
